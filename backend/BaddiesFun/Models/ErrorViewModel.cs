bd89202cb7a9d00299695e1abdb93e909ad5f74a
KamronRichards <kamronrichards2@gmail.com>
2023-03-31 22:24:09 -0600

    Mission 13

---
+++ backend/BaddiesFun/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BaddiesFun.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
